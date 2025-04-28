26cb74b70d96e1915ddf8cf92795504c2fd1a2f6
Samuel Huxtable <64793716+Sam-Huxtable@users.noreply.github.com>
2025-04-28 15:54:44 +0100

    Update issi.c

--- drivers/mtd/nand/spi/issi.c
+++ drivers/mtd/nand/spi/issi.c
@@ -89,7 +89,7 @@ static int IS37SMW04G8B_ecc_get_status(struct spinand_device *spinand, uint8_t s
 static const struct spinand_info issi_spinand_table[] = {
     SPINAND_INFO("IS37SMW04G8B", 
              SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0x35),
-             NAND_MEMORG(1, 2048, 128, 64, 2048, 40, 1, 2, 1),
+             NAND_MEMORG(1, 2048, 128, 64, 2048, 40, 0, 2, 1),
              NAND_ECCREQ(8, 544),
 		         SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
 					                            &write_cache_variants,
